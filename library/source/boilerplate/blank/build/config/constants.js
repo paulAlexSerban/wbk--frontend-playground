0c913a70382acc024c91f0d704230091c72bcf65
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-12 19:18:55 +0300

    implemented boilerplate blank to library

---
+++ library/source/boilerplate/blank/build/config/constants.js
@@ -0,0 +1,5 @@
+export const PROJECT_NAME = "boilerplate__blank";
+export const PARENT_DIR = "library/source";
+export const PROJECT_PATH = "boilerplate/blank";
+export const SRC_DIR = `boilerplate/blank/source`;
+export const DIST_DIR = "./library/dist";
