d7bc931938d4de24169608799674699703b2bd0f
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-12 19:24:22 +0300

    added blank boilerplate for custom-video-player

---
+++ library/source/video/basic-custom-player/build/config/constants.js
@@ -0,0 +1,5 @@
+export const PROJECT_NAME = "video__basic-custom-player";
+export const PARENT_DIR = "library/source";
+export const PROJECT_PATH = "video/basic-custom-player";
+export const SRC_DIR = `video/basic-custom-player/source`;
+export const DIST_DIR = "./library/dist";
