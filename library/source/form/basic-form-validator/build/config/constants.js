dcf7dbae0d4bb4e80f84d3e72d698f71d3feb1bc
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-13 14:41:11 +0300

    updated basicFormNaming

--- library/source/form/basic-form-validator/build/config/constants.js
+++
@@ -1,5 +0,0 @@
-export const PROJECT_NAME = "form__basic-form-validator";
-export const PARENT_DIR = "library/source";
-export const PROJECT_PATH = "form/basic-form-validator";
-export const SRC_DIR = `form/basic-form-validator/source`;
-export const DIST_DIR = "./library/dist";
---
+++ library/source/form/basicFormValidator/build/config/constants.js
@@ -0,0 +1,5 @@
+export const PROJECT_NAME = "form__basicFormValidator";
+export const PARENT_DIR = "library/source";
+export const PROJECT_PATH = "form/basicFormValidator";
+export const SRC_DIR = `form/basicFormValidator/source`;
+export const DIST_DIR = "./library/dist";
