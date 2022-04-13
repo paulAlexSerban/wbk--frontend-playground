e9c242722ce6c2a53d0140d4e1909a0f631bfffc
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-13 11:20:14 +0300

    added build for rotating navigation

---
+++ library/source/navigation/rotating-navigation/build/config/constants.js
@@ -0,0 +1,5 @@
+export const PROJECT_NAME = "navigation__rotating-navigation";
+export const PARENT_DIR = "library/source";
+export const PROJECT_PATH = "navigation/rotating-navigation";
+export const SRC_DIR = `navigation/rotating-navigation/source`;
+export const DIST_DIR = "./library/dist";
