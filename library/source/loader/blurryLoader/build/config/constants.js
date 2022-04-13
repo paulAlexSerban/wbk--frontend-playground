bf1b961527f59cace6d565599d04c4437128215a
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-13 13:31:17 +0300

    added build for blurry laoder

---
+++ library/source/loader/blurryLoader/build/config/constants.js
@@ -0,0 +1,5 @@
+export const PROJECT_NAME = "loader__blurryLoader";
+export const PARENT_DIR = "library/source";
+export const PROJECT_PATH = "loader/blurryLoader";
+export const SRC_DIR = `loader/blurryLoader/source`;
+export const DIST_DIR = "./library/dist";
