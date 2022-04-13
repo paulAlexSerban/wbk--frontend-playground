b13a7d44e4d3fb076915ba77c415c82c4e25a257
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-13 10:36:47 +0300

    added progress-steps build

---
+++ library/source/steps/progress-steps/build/config/constants.js
@@ -0,0 +1,5 @@
+export const PROJECT_NAME = "steps__progress-steps";
+export const PARENT_DIR = "library/source";
+export const PROJECT_PATH = "steps/progress-steps";
+export const SRC_DIR = `steps/progress-steps/source`;
+export const DIST_DIR = "./library/dist";
