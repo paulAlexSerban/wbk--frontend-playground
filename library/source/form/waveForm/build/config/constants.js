503b44e45936ae96b9481ac94c27f8adbaad2201
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-13 14:56:32 +0300

    added build for wave form

---
+++ library/source/form/waveForm/build/config/constants.js
@@ -0,0 +1,5 @@
+export const PROJECT_NAME = "form__waveForm";
+export const PARENT_DIR = "library/source";
+export const PROJECT_PATH = "form/waveForm";
+export const SRC_DIR = `form/waveForm/source`;
+export const DIST_DIR = "./library/dist";
