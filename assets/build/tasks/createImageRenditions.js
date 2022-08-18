e5582f386b0bbaebecfee66e356a4ec929089c45
paulAlexSerban <paul.alex.serban@gmail.com>
2022-08-18 20:30:38 +0300

    feat: add assets

--- assets/build/tasks/createImageRenditions.js
+++ assets/build/tasks/createImageRenditions.js
@@ -1,48 +1,81 @@
 import gulpResponsive from "gulp-responsive";
 import { src, dest } from "gulp";
 import plumber from "gulp-plumber";
+import { paths } from "../config/paths";
+import size from "gulp-size";
+import { onError } from "../utils/onError";
 
 const imgRenditionConf = {
   "*": [
     {
+      width: 320,
+      rename: {
+        suffix: "-320px",
+        extname: ".webp",
+      },
+    },
+    {
       width: 480,
-      rename: { 
-        suffix: "-480px", 
-        extname: ".webp" },
+      rename: {
+        suffix: "-480px",
+        extname: ".webp",
+      },
     },
     {
       width: 960,
-      rename: { 
-        suffix: "-960px", 
-        extname: ".webp" },
+      rename: {
+        suffix: "-960px",
+        extname: ".webp",
+      },
     },
     {
       width: 1440,
-      rename: { 
-        suffix: "-1440px", 
-        extname: ".webp" },
+      rename: {
+        suffix: "-1440px",
+        extname: ".webp",
+      },
     },
     {
-      rename: { 
-        suffix: "-original", 
-        extname: ".webp" },
+      width: 1920,
+      rename: {
+        suffix: "-1920px",
+        extname: ".webp",
+      },
     },
-  ]
+    {
+      rename: {
+        suffix: "-original",
+        extname: ".webp",
+      },
+    },
+  ],
 };
 
-const originalImageEntries = "./source/images/*";
-const renditionsDir = "./dist/images";
-
 export const createImageRenditions = () => {
-  return src(originalImageEntries)
-    .pipe(plumber())
-    .pipe(
-      gulpResponsive(imgRenditionConf, {
-        quality: 85,
-        progressive: true,
-        withMetadata: false,
-        withoutEnlargement: false
-      })
-    )
-    .pipe(dest(renditionsDir));
+  return new Promise((resolve, reject) => {
+    return src(paths.src.assets.images)
+      .pipe(
+        plumber({
+          errorHandler: onError,
+        })
+      )
+      .pipe(
+        gulpResponsive(imgRenditionConf, {
+          quality: 85,
+          progressive: true,
+          withMetadata: false,
+          withoutEnlargement: false,
+        })
+      )
+      .pipe(
+        size({
+          title: "createImageRenditions : ",
+          showFiles: true,
+          showTotal: true,
+        })
+      )
+      .pipe(dest(`${paths.dist.dir}/images`))
+      .on("error", reject)
+      .on("end", resolve);
+  });
 };
