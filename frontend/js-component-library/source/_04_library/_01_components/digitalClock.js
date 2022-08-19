bd898e158796c6d3245b79d77b838d724a53b918
paulAlexSerban <paul.alex.serban@gmail.com>
2022-08-19 22:47:50 +0300

    feat(digital-clock): add digital clock

--- frontend/js-component-library/source/_04_library/_01_components/digitalClock.js
+++
@@ -1,5 +0,0 @@
-import { test } from "../_01_variables/variables";
-
-export const digitalClock = () => {
-  console.log(`digital clocks- ${test}`);
-};
--- frontend/js-component-library/source/_05_system/_02_pages/index/index.page.layer.js
+++ frontend/js-component-library/source/_05_system/_02_pages/index/index.page.layer.js
@@ -1,4 +1,3 @@
 import { config } from "./config";
 
-console.log("hello INDEX PAGE");
-console.log(config.pageName);
+console.log(`${config.pageName} page initialized`);
