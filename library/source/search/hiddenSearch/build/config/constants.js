b8f95ebab284711806c0136133a3c8d9150aba80
paulAlexSerban <paul.alex.serban@gmail.com>
2022-08-30 12:32:21 +0300

    feat(search): remove old code

--- library/source/search/hiddenSearch/build/config/constants.js
+++
@@ -1,5 +0,0 @@
-export const PROJECT_NAME = "search__hiddenSearch";
-export const PARENT_DIR =  "source";
-export const PROJECT_PATH = "search/hiddenSearch";
-export const SRC_DIR = `search/hiddenSearch/source`;
-export const DIST_DIR = "./dist";
