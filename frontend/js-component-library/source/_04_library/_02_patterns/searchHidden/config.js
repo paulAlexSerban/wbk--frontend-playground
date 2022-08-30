7faf13822f5d80fb70c0123b14863b2eab3f1028
paulAlexSerban <paul.alex.serban@gmail.com>
2022-08-30 13:43:31 +0300

    feat(search): add search hidden code

---
+++ frontend/js-component-library/source/_04_library/_02_patterns/searchHidden/config.js
@@ -0,0 +1,12 @@
+export const config = {
+  type: "pat",
+  name: "SearchHidden",
+  selectors: {
+    search: ".js-search",
+    button: ".js-search-btn",
+    input: ".js-search-input"
+  },
+  states: {
+    active: "active",
+  },
+};
