fb076029f60476e956ec8b3791e377d5f1603840
paulAlexSerban <paul.alex.serban@gmail.com>
2023-05-10 10:44:18 +0300

    feat(modulize): implemented modulize to automate tasks

---
+++ frontend/component-library/scripts/install.bash
@@ -0,0 +1,8 @@
+#!/bin/bash 
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+source ../config.env
+
+rm -rfv ../node_modules
+npm --prefix .. install
