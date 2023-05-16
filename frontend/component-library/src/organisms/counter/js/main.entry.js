2c1e2c96d6398187cbf1492c7e6f3fea32a953a1
paulAlexSerban <paul.alex.serban@gmail.com>
2023-05-16 19:00:55 +0300

    feat(counter): added counter

---
+++ frontend/component-library/src/organisms/counter/js/main.entry.js
@@ -0,0 +1,36 @@
+import { stopBubbling } from "../../../_commons/js/functions/stopBubbling";
+
+class Counter {
+    constructor() {
+        this.init();
+    }
+
+    bumpCounter() {
+        this.currentVal = parseInt(this.counterOutput.innerHTML, 10);
+        this.newVal = this.currentVal + 1;
+        this.counterOutput.innerHTML = this.newVal;
+    }
+
+    setupEventListeners() {
+        this.counterButton.addEventListener("click", (event) => {
+            stopBubbling(event);
+            this.bumpCounter();
+        });
+    }
+
+    setupDOMReferences() {
+        this.counterButton = document.querySelector(".counter__button");
+        this.counterOutput = document.querySelector(".counter__output");
+    }
+
+    init() {
+        this.setupDOMReferences();
+        this.setupEventListeners();
+    }
+}
+
+(() => {
+    document.querySelectorAll('[data-js-component="Counter"]').forEach((molecule) => {
+        new Counter(molecule);
+    });
+})();
