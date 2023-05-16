14e8792bf11779fcbb3f1547dd7bf42a4a4874c3
paulAlexSerban <paul.alex.serban@gmail.com>
2023-05-16 16:08:10 +0300

    feat: added math game

--- frontend/component-library/src/organisms/mini-games/js/main.entry.js
+++ frontend/component-library/src/organisms/mini-games/js/main.entry.js
@@ -1,24 +1,25 @@
-import { GuessMyNumber, config as GMNConfig } from './GuessMyNumber.js';
-import { NumberGuesser, config as NGConfig } from './NumberGuesser.js';
-import { CoinGame, config as CGConfig } from './CoinGame.js';
+import { GuessMyNumber, config as GMNConfig } from './_GuessMyNumber.js';
+import { NumberGuesser, config as NGConfig } from './_NumberGuesser.js';
+import { CoinGame, config as CGConfig } from './_CoinGame.js';
+import { MathGame, config as MConfig } from './_MathGame.js';
+
 
 (() => {
     document.querySelectorAll(`[data-js-${GMNConfig.type}=${GMNConfig.name}]`).forEach((el) => {
         GuessMyNumber(el);
     });
-})();
 
+    document.querySelectorAll(`[data-js-${NGConfig.type}=${NGConfig.name}]`).forEach((el) => {
+        NumberGuesser(el);
+    });
 
-(() => {
-  document.querySelectorAll(`[data-js-${NGConfig.type}=${NGConfig.name}]`).forEach((el) => {
-      NumberGuesser(el);
-  });
-})();
-
-
-
-(() => {
     document.querySelectorAll(`[data-js-${CGConfig.type}=${CGConfig.name}]`).forEach((el) => {
         CoinGame(el);
     });
+
+    document.querySelectorAll(`[data-js-${MConfig.type}=${MConfig.name}]`).forEach((el) => {
+        MathGame(el);
+    });
 })();
+
+
