676ae6a4748c9c07760d62d8f9299f2b1eab80d7
paulAlexSerban <paul.alex.serban@gmail.com>
2023-05-16 21:54:22 +0300

    feat(to do list): added to do list v2 - mvc

--- frontend/component-library/src/organisms/to-do-list/js/main.entry.js
+++ frontend/component-library/src/organisms/to-do-list/js/main.entry.js
@@ -1,5 +1,6 @@
 import { TaskList, config as TLConfig } from "./_TaskList";
 import { ToDoListApp, config as TDLConfig } from "./_ToDoListApp";
+import ToDoListAppV2 from "./_ToDoListAppV2";
 
 (() => {
     document.querySelectorAll(`[data-js-${TLConfig.type}=${TLConfig.name}]`).forEach((el) => {
@@ -9,4 +10,6 @@ import { ToDoListApp, config as TDLConfig } from "./_ToDoListApp";
     document.querySelectorAll(`[data-js-${TDLConfig.type}=${TDLConfig.name}]`).forEach((el) => {
         ToDoListApp(el);
     });
+
+    ToDoListAppV2();
 })();
