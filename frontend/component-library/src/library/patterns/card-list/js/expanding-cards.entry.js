e0278eaf7eb8590bdf5336bcd05418fb3844bb18
paulAlexSerban <paul.alex.serban@gmail.com>
2023-06-01 02:35:27 +0300

    feat(patterns): card-list

---
+++ frontend/component-library/src/library/patterns/card-list/js/expanding-cards.entry.js
@@ -0,0 +1,49 @@
+import { find } from "../../../../_abstracts/js/dom/traversing";
+import { addClass, removeClass } from "../../../../_abstracts/js/dom/manipulation";
+
+(() => {
+    const config = {
+        type: "pat",
+        name: "ExpandingCards",
+        selectors: {
+            cardPanel: ".js-expanding-cards-panel",
+        },
+        states: {
+            panelActive: "active",
+        },
+    };
+
+    const ExpandingCards = (el) => {
+        const global = { state: {}, elements: {} };
+
+        const init = () => {
+            setupDomReferences();
+            setupEventListeners();
+        };
+
+        const removeActiveClasses = () => {
+            global.elements.cardPanels.forEach((panel) => {
+                removeClass(config.states.panelActive, panel);
+            });
+        };
+
+        const setupEventListeners = () => {
+            global.elements.cardPanels.forEach((panel) => {
+                panel.addEventListener("click", () => {
+                    removeActiveClasses();
+                    addClass(config.states.panelActive, panel);
+                });
+            });
+        };
+
+        const setupDomReferences = () => {
+            global.elements.cardPanels = find(config.selectors.cardPanel, el);
+        };
+
+        init();
+    };
+
+    document.querySelectorAll(`[data-js-${config.type}=${config.name}]`).forEach((el) => {
+        ExpandingCards(el);
+    });
+})();
