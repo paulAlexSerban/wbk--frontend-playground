282f335145bcca7b0cf71c1e342508f023343008
paulAlexSerban <paul.alex.serban@gmail.com>
2023-07-25 15:06:20 +0300

    ci: log base url

--- frontend/component-library/build/helpers/handlebars/feLibs.js
+++ frontend/component-library/build/helpers/handlebars/feLibs.js
@@ -1,13 +1,12 @@
-const process = require("process");
 
+const { env } = require("process");
+console.log(env);
 module.exports = function (group, category, name, variation, type) {
-    const path = `${process.env.BASE_URL}${
-        process.env.BASE_URL.length !== 0 ? "/component-library" : ""
-    }/${group}/${category}/${name}/${variation}`;
+    const path = `${env.BASE_URL}${ env.BASE_URL.length !== 0 ? "/component-library" : "" }/${group}/${category}/${name}/${variation}`;
 
     const feLibs = {
         css: `<link rel="stylesheet" href="${path}.css">`,
-        js: `<script src="${path}.js" async defer></script>`,
+        js:  `<script src="${path}.js" async defer></script>`,
         all: `<link rel="stylesheet" href="${path}.css"><script src="${path}.js" async defer></script>`,
     };
 
