b00d0c6496b25acc6e73c454f3807bd41c32940e
paulAlexSerban <paul.alex.serban@gmail.com>
2023-07-25 14:42:36 +0300

    ci: remove env variable log

--- frontend/dashboard/src/core/commons/paths.js
+++ frontend/dashboard/src/core/commons/paths.js
@@ -1,7 +1,5 @@
 const { env } = require("process");
 
-console.log("env.INTERNAL_COMPONENT_LIBRARY_URL", env);
-
 const paths = {
     internalComponentLibraryUrl: env.INTERNAL_COMPONENT_LIBRARY_URL,
     userComponentLibraryUrl: env.USER_COMPONENT_LIBRARY_URL,
