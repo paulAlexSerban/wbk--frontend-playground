269c7beb279a42e2a6ea4bea6aba5a0020c1a2e0
paulAlexSerban <paul.alex.serban@gmail.com>
2023-07-31 09:41:52 +0300

    refactor(config): refactor

--- backend/proxy/scripts/docker-start.bash
+++ backend/proxy/scripts/docker-start.bash
@@ -2,7 +2,6 @@
 # makes sure the folder containing the script will be the root folder
 cd "$(dirname "$0")" || exit
 
-bash generate-ssl-certs.bash
 echo "🟢  START 🐳  Traefik Proxy"
 docker-compose --env-file ../../../.env.development \
   --file ../docker-compose.dev.yml \
