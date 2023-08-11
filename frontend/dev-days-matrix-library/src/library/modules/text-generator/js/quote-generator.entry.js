a727967ab48f96dd914a58cb7a36ed38cc8e1571
paulAlexSerban <paul.alex.serban@gmail.com>
2023-08-11 14:02:20 +0300

    feat: updated quote generator json api

--- frontend/dev-days-matrix-library/src/library/modules/text-generator/js/quote-generator.entry.js
+++ frontend/dev-days-matrix-library/src/library/modules/text-generator/js/quote-generator.entry.js
@@ -44,7 +44,7 @@ function newQuote() {
 
 // Get Quotes from API
  async function getQuotes(){
-     const apiUrl = 'https://type.fit/api/quotes';
+     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
      try{
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
