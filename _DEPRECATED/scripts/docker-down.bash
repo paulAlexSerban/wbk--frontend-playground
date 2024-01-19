ff8d18452de786bd69d378c7df6a5fa6bf0523a5
paulAlexSerban <paul.alex.serban@gmail.com>
2024-01-19 14:10:31 +0200

    feat(rename and added new library): added html5up research library

--- _DEPRECATED/scripts/docker-down.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# makes sure the folder containing the script will be the root folder
-cd "$(dirname "$0")" || exit
-
-echo " 🛑  STOP 🐳 Nginx Generic Component Library"
-docker-compose --env-file ../../../.env.development \
-  --file ../docker-compose.dev.yml \
- down --volumes --rmi all
---
+++ frontend/rnd-html5up-library/build/loaders/scss.js
@@ -0,0 +1,20 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const scss = {
+    // use CSS and Sass loaders to compile CSS stylesheets
+    test: /\.(sa|sc|c)ss$/,
+    use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        {
+            loader: 'sass-loader',
+            options: {
+                // Prefer `dart-sass`
+                implementation: require('sass'),
+                additionalData: `$assetsPath: '${process.env.ASSETS_PATH}';`,
+            },
+        },
+    ],
+};
+
+module.exports = scss;
