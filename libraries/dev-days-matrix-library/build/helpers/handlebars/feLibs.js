743617ae886bcf5d2fa7cdb193581c0e805fe01c
paulAlexSerban <paul.alex.serban@gmail.com>
2025-06-13 22:50:37 +0300

    feat: fix dev days matrix issues

--- libraries/dev-days-matrix-library/build/helpers/handlebars/feLibs.js
+++ libraries/dev-days-matrix-library/build/helpers/handlebars/feLibs.js
@@ -1,9 +1,14 @@
 const process = require('process');
-const { env } = process;
-const projectName = require('../../../package.json').name.split('/')[1];
-module.exports = function (group, category, name, variation, type) {
-    const path = `${env.BASE_URL}${projectName}/${group}/${category}/${name}/${variation}`;
+const projectName = require('../../../package.json').name;
 
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = function (group, category, name, variation, type) {
+    const path =
+        NODE_ENV === 'development'
+            ? `/${group}/${category}/${name}/${variation}`
+            : `${BASE_URL}/${projectName}/${group}/${category}/${name}/${variation}`;
     const feLibs = {
         css: `<link rel="stylesheet" href="${path}.css">`,
         js: `<script src="${path}.js" defer></script>`,
