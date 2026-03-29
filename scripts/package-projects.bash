15217d856e1bb983849035738a77ae8659b993a7
paulAlexSerban <paul.alex.serban@gmail.com>
2026-03-29 13:59:24 +0300

    feat: package projects script

---
+++ scripts/package-projects.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+# clean up previous packages
+rm -rfv ../package/*
+
+REPOSITORY_NAME=$(node -p "require('../package.json').name.split('/')[0].split('@')[1]")
+echo "📦  Packaging projects for $REPOSITORY_NAME"
+
+# Loop through all apps in the libraries directory
+for PROJECT_PATH in ../projects/*/*; do
+  echo "📦  Packaging $PROJECT_PATH"
+  if [[ -d "$PROJECT_PATH/dist" ]]; then
+    PROJECT_CATEGORY=$(node -p "require('$PROJECT_PATH/package.json').name.split('/')[1].split('__')[0]")
+    PROJECT_NAME=$(node -p "require('$PROJECT_PATH/package.json').name.split('/')[1].split('__')[1]")
+    
+    echo "📦  Package $PROJECT_NAME"
+    mkdir -p ../package/$REPOSITORY_NAME/projects/$PROJECT_CATEGORY/$PROJECT_NAME
+    cp -rfv "$PROJECT_PATH/dist/"* ../package/$REPOSITORY_NAME/projects/$PROJECT_CATEGORY/$PROJECT_NAME
+  fi
+done
