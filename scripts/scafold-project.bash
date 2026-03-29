b88c55f4409aaff643eb1cb74070183d85f25653
paulAlexSerban <paul.alex.serban@gmail.com>
2026-03-29 22:00:02 +0300

    feat: landing page menu slide

--- scripts/scafold-project.bash
+++ scripts/scafold-project.bash
@@ -1,8 +1,8 @@
-PROJECT_NAME="Landing Page Animated Tawilwind Template"
+PROJECT_NAME="Landing Page Menu Slide"
 PROJECT_CATEGORY="reverse-engineering"
 PROJECT_TEMPLATE="handlebars-template"
-PROJECT_SLUG="landing-page-animated-tawilwind-template"
-PROJECT_DESCRIPTION="Landing page built with Tailwind CSS"
+PROJECT_SLUG="landing-page-menu-slide"
+PROJECT_DESCRIPTION="Landing page with a menu that slides in from the left. The menu is hidden by default and can be toggled by clicking on the menu icon."
 PROJECT_SOURCE="JavaScript Web Projects: 20 Projects to Build Your Portfolio"
 PROJECT_SOURCE_URL="https://www.udemy.com/course/javascript-web-projects-to-build-your-portfolio-resume/"
 
