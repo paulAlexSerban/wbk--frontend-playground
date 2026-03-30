cb6f1da6f25f8bd03216657bbd929013bfb9b117
paulAlexSerban <paul.alex.serban@gmail.com>
2026-03-30 16:11:01 +0300

    feat: alert

---
+++ projects/components/alert/build/helpers/handlebars/feLibs.js
@@ -0,0 +1,16 @@
+
+
+module.exports = function (type) {
+    const feLibs = {
+        css: `<link rel="stylesheet" href="styles.css">`,
+        js: `<script src="scripts.js" defer></script>`,
+        all: `<link rel="stylesheet" href="styles.css">
+             <script src="scripts.js" defer></script>`,
+    };
+
+    console.log('\n');
+    feLibs[type].split('\n').forEach(line => {
+        console.log('[ handlebars - INFO ]: injecting FE library:', line.trim());
+    });
+    return feLibs[type];
+};
